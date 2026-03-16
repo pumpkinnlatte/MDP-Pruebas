68363796a3649aebe9dc450ad2922c4e5752bbd8
pumpkinnlatte <2230343@upv.edu.mx>
2026-03-16 13:44:30 -0600

    CLONE from actual mdpprolog

--- src/fluent/__init__.py
+++
@@ -1,8 +0,0 @@
-from src.fluent.schema import Fluent, FluentSchema
-from src.fluent.spaces import FactorSpace, StateSpace, ActionSpace
-from src.fluent.classification import FluentClassifier
-from src.fluent.exceptions import (
-    MDPProbLogError, FluentDeclarationError,
-    FluentInferenceError, FluentCardinalityError,
-    FluentMassConservationError
-)
